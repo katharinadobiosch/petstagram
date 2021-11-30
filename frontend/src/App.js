85d8f441313506980f50affe2e278652441c22d7
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2021-11-30 18:43:35 +0000

    added navbar and paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,8 @@
-// import React from "react";
-// import {
-//     BrowserRouter as Router,
-//     Route,
-//     Redirect,
-//     Switch,
-// } from "react-router-dom";
-
-// import Navbar from "./components/Navbar";
-// import "./App.css";
-
-// import Home from "./components/views/Home";
-// import Login from "./components/views/Login";
-// import Profile from "./components/views/Profile";
-// import Signup from "./components/views/Signup";
-
-// const App = () => {
-//     return (
-//         <>
-//             <Router>
-//                 <Navbar />
-//                 <main>
-//                     <Routes>
-//                         <Route path="/" exact component={Home} />
-
-//                         <Route path="/signup" exact component={Signup} />
-
-//                         <Route path="/login" exact component={Login} />
-
-//                         <Route path="/profile" exact component={Profile} />
-//                     </Routes>
-//                 </main>
-//             </Router>
-//         </>
-//     );
-// };
-
-// export default App;
-
-////////////////////////////////////////////////////////
-
 import React from "react";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import "./App.css";
@@ -57,15 +19,15 @@ const App = () => {
                 <Navbar />
                 <main>
                     <Routes>
-                        <Route path="/" exact component={Home} />
+                        <Route exact path="/" element={<Home />} />
 
-                        <Route path="/login" exact component={Login} />
+                        <Route exact path="/login" element={<Login />} />
 
-                        <Route path="/signup" exact component={Signup} />
+                        <Route exact path="/signup" element={<Signup />} />
 
-                        <Route path="/profile" exact component={Profile} />
+                        <Route exact path="/profile" element={<Profile />} />
 
-                        {/* <Redirect to="/" /> */}
+                        <Route path="*" element={<Home />} />
                     </Routes>
                 </main>
             </Router>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
                             <a href="/login">Log in</a>
                         </li>
                         <li>
-                            <a href="/signup">Sign in</a>
+                            <a href="/signup">Sign up</a>
                         </li>
                         <li>
                             <a href="/profile">Profile</a>
